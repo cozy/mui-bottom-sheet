4f764a32ff8ff6f398e981a8298471453b9e8577
JF-Cozy <67680939+JF-Cozy@users.noreply.github.com>
2022-07-27 15:50:57 +0200

    Add dist files via direct upload

---
+++ dist/util.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Find the closest value in an array to a given value
+ * @param goal target value
+ * @param numbers array of numbers to search through
+ */
+export declare const closest: (goal: number, numbers: number[]) => number;
+/**
+ * Find the next value in an array to a given value
+ * @param goal target value
+ * @param direction indicates whether to find for a higher or lower value
+ * @param numbers array of numbers to search through
+ */
+export declare const next: (goal: number, direction: number, numbers: number[]) => number;
